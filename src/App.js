dc69d8e202a2b0cd767c755538aa3fbb60ab2ffe
juanosorioh <juangabrielosorioheredia@gmail.com>
2021-11-26 16:19:08 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Des from "./pages/Des";
 
 function App() {
+
   return (
     <>
+    <Navbar/>
+    <Des/>
     </>
   );
 }
---
+++ src/pages/Des.js
@@ -0,0 +1,48 @@
+import React from "react";
+import logo from "../assets/bootstrap-logo.svg";
+
+const s = () => {
+  return (
+    <div>
+      <main className="form-signin mx-auto w-25 my-5">
+        <form>
+          <div className="row mx-auto w-25 my-5">
+          <img className="mb-4" src={logo} alt="" width="25" height="45" />
+          </div>
+          <h1 className="h3 mb-3 fw-normal text-center">Please sign in</h1>
+
+          <div className="form-floating my-3">
+            <input
+              type="email"
+              className="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div className="form-floating my-3">
+            <input
+              type="password"
+              className="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label for="floatingPassword">Password</label>
+          </div>
+
+          <div className="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me" /> Remember me
+            </label>
+          </div>
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
+            Sign in
+          </button>
+          <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default s;
